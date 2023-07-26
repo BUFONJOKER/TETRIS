78b84cdf57c35d24f9eb592a481499622889b29f
ABDUL REHMAN JAVAID <S21BDOCS1M01013@iub.edu.pk>
2023-07-26 17:09:39 +0500

    comments added

--- score.py
+++ score.py
@@ -6,13 +6,16 @@ class Score:
     # init function to initialize the score
     def __init__(self):
 
+        # set the surface
         self.surface = pygame.Surface((sidebar_width,score_height_fraction * game_height))
+        
+        # set the display surface
         self.display_surface = pygame.display.get_surface()
 
+    # function to run the score
     def run(self):
 
         # draw the surface
-        # add padding at lower end
         self.display_surface.blit(self.surface, (game_width + 2 * padding, padding + preview_height_fraction * game_height))
 
        
