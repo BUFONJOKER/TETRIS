f76b500d9c14e9712a07d3d2edc0aabdad44999c
ABDUL REHMAN JAVAID <S21BDOCS1M01013@iub.edu.pk>
2023-07-26 14:58:47 +0500

    spaces added

--- main.py
+++ main.py
@@ -3,41 +3,55 @@ from sys import exit
 
 # create a class for the main game
 class Main:
+
     # initialize the game
     def __init__(self):
         
         #general
         pygame.init()
+
         # set the display window
         self.display = pygame.display.set_mode((640,480))
+
         # set the clock
         self.clock = pygame.time.Clock()
+        
         # set the title of the window
         pygame.display.set_caption("Tetris")
     
     # function run the game
     def run(self):
+
         # game loop
         while True:
+            
             # check for events
             for event in pygame.event.get():
+                
                 # check if the event is the X button
                 if event.type == pygame.QUIT:
+                    
                     # quit the game
                     pygame.quit()
+                    
                     # exit the program
                     exit()
+            
             # fill screen with grey color
             self.display.fill((grey))
+            
             # Update the game
             pygame.display.update()
+            
             # set the fps
             self.clock.tick(60)
 
 # run the game
 if __name__ == "__main__":
+    
     # Create the game object
     main = Main()
+    
     # Run the game
     main.run()
 
